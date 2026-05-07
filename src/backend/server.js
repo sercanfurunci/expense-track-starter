585570568e5a69ce9353df1e86072103ce917013
Sercan Furunci <sercanfurunci41@gmail.com>
2026-05-07 19:39:27 +0300

    fix: return CORS rejection without 500 crash

--- src/backend/server.js
+++ src/backend/server.js
@@ -89,7 +89,7 @@ app.use(cors({
     ) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(null, false);
     }
   },
   credentials: true,
